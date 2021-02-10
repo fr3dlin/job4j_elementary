78e97636dd0c3c9917f63dd907e50658d8b76caf
fr3dlin <fr3dlin@gmail.com>
2021-02-11 00:30:00 +0300

    9.1. String [#470503]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -2,6 +2,11 @@ package ru.job4j.condition;
 
 public class Greeting {
     public  static void main(String[] args){
-
+        String idea = "I like java!";
+        System.out.println(idea);
+        int year = 2021;
+        idea = idea + " But i am a newbie.";
+        idea = idea + " " + year;
+        System.out.println(idea);
     }
 }
