8dc6ab5f0ee4ebed4aaa2c9a1145c0b4f303887d
fr3dlin <fr3dlin@gmail.com>
2021-02-10 23:13:11 +0300

    3.2. Фиксация изменений на github [#470520]

---
+++ src/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        Calculator.plus(1, 2);
+        Calculator.plus(10, 11);
+    }
+}
+
