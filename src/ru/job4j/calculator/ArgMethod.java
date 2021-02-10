ed593742c6db0617a593ec3f0e64581035b10576
fr3dlin <fr3dlin@gmail.com>
2021-02-10 23:15:04 +0300

    4.1. Аргументы [#470504]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -8,7 +8,7 @@ public class ArgMethod {
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
-        int age = 33;
+        int age = 31;
         ArgMethod.hello(name);
     }
 }
