0e24ecb9d2f5b597b8fada313437af08c7d24811
fr3dlin <fr3dlin@gmail.com>
2021-02-13 15:20:25 +0300

    1. Модульные тесты. [#470526]

---
+++ test/ru/job4j/calculator/ConverterTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenConvert120RblThen2Dlr() {
+        int in = 120;
+        int expected = 2;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+}
