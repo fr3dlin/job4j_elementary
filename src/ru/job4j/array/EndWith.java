c91a55d3045a5bd8492a1e7d4326183274c5d99a
fr3dlin <fr3dlin@gmail.com>
2021-02-21 14:35:00 +0300

    6.4.2. Слово заканчивается на ... [#470466]

--- src/ru/job4j/array/EndWith.java
+++ src/ru/job4j/array/EndWith.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class EndWith {
     public static boolean endsWith(char[] word, char[] post) {
         for (int i = 0; i < post.length; i++) {
-            if (post[post.length - 1] != word[word.length - 1]) {
+            if (post[post.length - 1 - i] != word[word.length - 1 - i]) {
                 return false;
             }
         }
