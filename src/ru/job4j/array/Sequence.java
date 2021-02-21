425baa1d7250b96572b1af9628f970bee82f47ae
fr3dlin <fr3dlin@gmail.com>
2021-02-21 14:16:34 +0300

    6.6.1. Двухмерный массив. Вывод на консоль. [#470480]

--- src/ru/job4j/array/Sequence.java
+++ src/ru/job4j/array/Sequence.java
@@ -12,5 +12,14 @@ public class Sequence {
         array[2][0] = 7;
         array[2][1] = 8;
         array[2][2] = 9;
+        System.out.println(array[0][0]);
+        System.out.println(array[0][1]);
+        System.out.println(array[0][2]);
+        System.out.println(array[1][0]);
+        System.out.println(array[1][1]);
+        System.out.println(array[1][2]);
+        System.out.println(array[2][0]);
+        System.out.println(array[2][1]);
+        System.out.println(array[2][2]);
     }
 }
